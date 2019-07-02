57e95bb23740f457023e9e58c4c38a5a387af63c
Cameron Prestera <cameronprestera@camerons-mbp.ad.uvu.edu>
2019-07-01 19:09:24 -0600

    for u babe

--- UVSimConsoleUI/Program.cs
+++ UVSimConsoleUI/Program.cs
@@ -7,6 +7,8 @@ namespace UVSimConsoleUI
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("hey gay bois");
+
         }
     }
 }
