8af8dd8b0edccacb62c604657e302b17883fa11f
Benjamin Thornhill <bthornhill123@gmail.com>
2019-07-10 16:45:08 -0600

    Comments about how to use Git for noob programmers

--- UVSimConsoleUI/Program.cs
+++ UVSimConsoleUI/Program.cs
@@ -2,6 +2,14 @@
 
 namespace UVSimConsoleUI
 {
+    // GIT Tutorial
+    // 1. <git pull> - pulls down latest version
+    // 2. Do some work
+    // 3. <git add .>
+    // 4. <git commit -m "Message goes here">
+    // 5. <git push>
+
+    // Note: <git status> will give you the current status
     class Program
     {
         static void Main(string[] args)
