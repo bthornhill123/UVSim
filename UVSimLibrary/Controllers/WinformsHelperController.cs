acf7bcca63ecf8b7a7ac214c1c2b81613480870a
Jaren Flaker <jarenflaker@gmail.com>
2019-07-27 15:49:37 -0600

    nigga

--- UVSimLibrary/Controllers/WinformsHelperController.cs
+++ UVSimLibrary/Controllers/WinformsHelperController.cs
@@ -11,19 +11,18 @@ namespace UVSimLibrary.Controllers
         {
             List<OperationModel> output = new List<OperationModel>();
 
-            // TODO - Fill these in with all of the 
-            output.Add(new OperationModel { Name = "", OpCode = "" });
-            output.Add(new OperationModel { Name = "", OpCode = "" });
-            output.Add(new OperationModel { Name = "", OpCode = "" });
-            output.Add(new OperationModel { Name = "", OpCode = "" });
-            output.Add(new OperationModel { Name = "", OpCode = "" });
-            output.Add(new OperationModel { Name = "", OpCode = "" });
-            output.Add(new OperationModel { Name = "", OpCode = "" });
-            output.Add(new OperationModel { Name = "", OpCode = "" });
-            output.Add(new OperationModel { Name = "", OpCode = "" });
-            output.Add(new OperationModel { Name = "", OpCode = "" });
-            output.Add(new OperationModel { Name = "", OpCode = "" });
-            output.Add(new OperationModel { Name = "", OpCode = "" });
+            output.Add(new OperationModel { Name = "Read", OpCode = "10" });
+            output.Add(new OperationModel { Name = "Write", OpCode = "11" });
+            output.Add(new OperationModel { Name = "Load", OpCode = "20" });
+            output.Add(new OperationModel { Name = "Store", OpCode = "21" });
+            output.Add(new OperationModel { Name = "Add", OpCode = "30" });
+            output.Add(new OperationModel { Name = "Subtract", OpCode = "31" });
+            output.Add(new OperationModel { Name = "Divide", OpCode = "32" });
+            output.Add(new OperationModel { Name = "Multiply", OpCode = "33" });
+            output.Add(new OperationModel { Name = "Branch", OpCode = "40" });
+            output.Add(new OperationModel { Name = "BranchNeg", OpCode = "41" });
+            output.Add(new OperationModel { Name = "BranchZero", OpCode = "42" });
+            output.Add(new OperationModel { Name = "Halt", OpCode = "43" });
 
             return output;
         }
